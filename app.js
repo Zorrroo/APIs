8de049e27b99b6fb66b0d492d2e9a6f645f61471
Prathamap290702 <prathamap2002@gmail.com>
2023-01-31 18:27:09 +0530

    Weather app done

--- app.js
+++ app.js
@@ -1,24 +1,33 @@
+const bodyParser = require('body-parser');
 const express = require('express');
 const https = require('https');
 const app = express()
 const port = 3000
-const url = "https://api.openweathermap.org/data/2.5/weather?q=Kalyan&units=metric&appid=7a7fa792ae46a8734b1b370a741f77c7"
 
-app.get('/', function(req, res) {
-    https.get(url, function(response){
-        console.log(response.statusCode);
-        response.on("data", function(data){
-            const weatherdata = JSON.parse(data);
-            const temp = weatherdata.main.temp;
-            const icon = data.weather[0].id;
-            const descp = weatherdata.weather[0].description;
-            const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png"
-            res.write("<h1>The temperature of kalyan is " + temp + " degress Celcius</h1>")
-            res.write("<h2>The weather seems to have a " + descp + ".</h2>");
-
+app.use(bodyParser.urlencoded({ extended: true }));
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + "/index.html")
+})
+app.post('/', function (req, res)   {
+    const location = req.body.cityname;
+    const appid = "7a7fa792ae46a8734b1b370a741f77c7";
+    const units = req.body.units;
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=" + location + "&units=" + units + "&appid=" + appid;
+        https.get(url, function (response) {
+            console.log(response.statusCode);
+            response.on("data", function (data) {
+                const weatherdata = JSON.parse(data);
+                const temp = weatherdata.main.temp;
+                const icon = weatherdata.weather[0].icon;
+                const descp = weatherdata.weather[0].description;
+                const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png"
+                res.write("<h1>The temperature of " + location + " is " + temp + " degress Celcius</h1>")
+                res.write("<h2>The weather seems to have a " + descp + ".</h2>");
+                res.write("<img src=" + imageURL + ">");
+                res.send();
+            })
         })
-    })      
-}
-);
+    }
+    );
 
 app.listen(port, () => console.log(`Server is running on port ${port}!`))       
